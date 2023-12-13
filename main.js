3163c05560c56a1e84333942a2c4e818c1592b97
Pietro Tomeo <pietrofrancescotomeo@gmail.com>
2023-12-13 21:48:01 +0100

    eserciziJs finiti

---
+++ main.js
@@ -0,0 +1,33 @@
+//ESERCIZIO 1
+// let number1 = 1
+// let number2 = 2
+// let number3 = 3
+// let number4 = 4
+// let number5 = 5
+// let somma = number1 + number2 + number3 + number4 + number5;
+// let media = somma / 5
+
+// console.log(`La somma tra i numeri equivale a ${somma} e la media risula ${media}`)
+
+
+//ESERCIZIO 2
+// let annocorrente = 2023
+// let annodinascita = 1997
+// let eta = annocorrente - annodinascita;
+// let annia100 = 100 - eta;
+
+// console.log(`Hai ${eta} anni e ti mancano ${annia100} anni per compiere 100.`)
+ 
+
+//ESERCIZIO 3
+// let numerogatti = 44
+// let numerogattiinfila = 6
+// let numerofile = 44 / 6 
+// let module = 44 % 6
+// let gattimancanti = 6 - 2
+// let gattiavanzati = 44 % 6
+
+// console.log(`Ci sono ${numerofile} file di gatti e ne mancano ${gattimancanti} per una nuova fila, con un avanzo di ${gattiavanzati}.`)
+
+
+
